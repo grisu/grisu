4451bbd578e8e629e759773f12a3390c65267465
Markus Binsteiner <markus.binsteiner@arcs.org.au>
2009-07-01 03:36:03 +0000

    Fixed build for grisu-grid-tests.jar file.
    git-svn-id: http://projects.arcs.org.au/svn/grisu/trunk/grisu@1410 8882a08a-ce34-0410-b810-d8c7c368afb5

--- fix-build.sh
+++ fix-build.sh
@@ -54,13 +54,14 @@ jar cmf ../../frontend/grisu-client-batch/MANIFEST.MF ../grisu-client-batch.jar 
 cd ..
 
 # fix grisu-grid-tests
-#mkdir grisu-grid-tests
-#cd grisu-grid-tests
-#unzip -o ../grisu-0.3-SNAPSHOT-grisu-grid-tests.jar
-##rm ../grisu-0.3-SNAPSHOT-grisu-client-swing.jar
-#rm -f META-INF/INDEX.LIST
-#cp ../../frontend/grisu-grid-tests/src/main/resources/log4j.properties .
-#jar cmf ../../frontend/grisu-grid-tests/MANIFEST.MF ../grisu-grid-tests.jar .
-#cd ..
+mkdir grisu-grid-tests
+cd grisu-grid-tests
+unzip -o ../grisu-0.3-SNAPSHOT-grisu-grid-tests.jar
+#rm ../grisu-0.3-SNAPSHOT-grisu-client-swing.jar
+rm -f META-INF/INDEX.LIST
+cp ../../frontend/grisu-grid-tests/src/main/resources/log4j.properties .
+cp ../../backend/grisu-core/src/main/resources/gluev12r2-ext-class-map.properties .
+jar cmf ../../frontend/grisu-grid-tests/MANIFEST.MF ../grisu-grid-tests.jar .
+cd ..
 
 
