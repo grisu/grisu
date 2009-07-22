f34a139d17013ac48c366aad346c9df21beef071
Markus Binsteiner <markus.binsteiner@arcs.org.au>
2009-07-22 07:40:39 +0000

    Changed maven multi module structure.
    git-svn-id: http://projects.arcs.org.au/svn/grisu/branches/enunciate-refactoring@1499 8882a08a-ce34-0410-b810-d8c7c368afb5

--- frontend/grisu-client-commandline/src/main/java/org/vpac/grisu/client/model/template/CommandLineParseException.java
+++
@@ -1,9 +0,0 @@
-package org.vpac.grisu.client.model.template;
-
-public class CommandLineParseException extends Exception {
-
-	public CommandLineParseException(String message) {
-		super(message);
-	}
-	
-}
