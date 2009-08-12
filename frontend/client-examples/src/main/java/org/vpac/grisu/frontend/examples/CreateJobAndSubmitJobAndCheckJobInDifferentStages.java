e27b92780ad5f3178bfedfc3dcddb2b1fe82ab53
Markus Binsteiner <markus.binsteiner@arcs.org.au>
2009-08-12 11:41:18 +1000

    Created new client-examples project.

---
+++ frontend/client-examples/src/main/java/org/vpac/grisu/frontend/examples/CreateJobAndSubmitJobAndCheckJobInDifferentStages.java
@@ -0,0 +1,76 @@
+package org.vpac.grisu.frontend.examples;
+
+import org.vpac.grisu.control.ServiceInterface;
+import org.vpac.grisu.frontend.control.login.LoginHelpers;
+import org.vpac.grisu.frontend.model.job.JobObject;
+import org.vpac.grisu.utils.SeveralXMLHelpers;
+
+import au.org.arcs.jcommons.constants.Constants;
+
+public final class CreateJobAndSubmitJobAndCheckJobInDifferentStages {
+
+	private CreateJobAndSubmitJobAndCheckJobInDifferentStages() {
+	}
+	
+	/**
+	 * @param args
+	 */
+	public static void main(final String[] args) throws Exception {
+
+		ServiceInterface si = LoginHelpers.login();
+
+		JobObject createJobObject = new JobObject(si);
+
+		createJobObject.setApplication("Java");
+		createJobObject.setCommandline("java -version");
+		createJobObject.setWalltimeInSeconds(3600 * 24 * 40);
+		createJobObject.setCpus(1);
+		createJobObject.addInputFileUrl("/home/markus/test.txt");
+		createJobObject
+				.addInputFileUrl("gsiftp://ng2.canterbury.ac.nz/home/grid-admin/C_AU_O_APACGrid_OU_VPAC_CN_Markus_Binsteiner/grix_splash_v1.1.jpg");
+
+		// GrisuRegistry registry = GrisuRegistry.getDefault(si);
+		// System.out.println(StringUtils.join(registry.getApplicationInformation("java").getAvailableSubmissionLocationsForFqan("/ARCS/NGAdmin"),"\n"));
+
+		System.out.println(SeveralXMLHelpers
+				.toStringWithoutAnnoyingExceptions(createJobObject
+						.getJobDescriptionDocument()));
+
+		String newJobname = createJobObject.createJob("/ARCS/NGAdmin",
+				Constants.TIMESTAMP_METHOD);
+
+		JobObject submitJobObject = new JobObject(si, newJobname);
+
+		System.out.println("Application: " + submitJobObject.getApplication());
+
+		submitJobObject.submitJob();
+
+		final JobObject checkJobObject = new JobObject(si, newJobname);
+
+		new Thread() {
+			public void run() {
+				try {
+					Thread.sleep(20000);
+					System.out.println("Sleeping over.");
+					checkJobObject.stopWaitingForJobToFinish();
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}.start();
+
+		boolean finished = checkJobObject.waitForJobToFinish(3);
+
+		if (!finished) {
+			System.out.println("not finished yet.");
+			checkJobObject.kill(true);
+		} else {
+			System.out.println("Stdout: " + checkJobObject.getStdOutContent());
+			System.out.println("Stderr: " + checkJobObject.getStdErrContent());
+			checkJobObject.kill(true);
+		}
+
+	}
+
+}
---
+++ frontend/client-examples/src/main/java/org/vpac/grisu/frontend/examples/JobCreationInfo.java
@@ -0,0 +1,43 @@
+package org.vpac.grisu.frontend.examples;
+
+import org.vpac.grisu.control.ServiceInterface;
+import org.vpac.grisu.control.exceptions.ServiceInterfaceException;
+import org.vpac.grisu.frontend.control.login.LoginParams;
+import org.vpac.grisu.frontend.control.login.ServiceInterfaceFactory;
+import org.vpac.grisu.model.GrisuRegistry;
+import org.vpac.grisu.model.GrisuRegistryManager;
+import org.vpac.grisu.model.dto.DtoJobs;
+import org.vpac.grisu.model.info.ApplicationInformation;
+
+public final class JobCreationInfo {
+	
+	private JobCreationInfo() {
+	}
+
+	public static void main(final String[] args) throws ServiceInterfaceException {
+
+		String username = args[0];
+		char[] password = args[1].toCharArray();
+
+		LoginParams loginParams = new LoginParams(
+		// "http://localhost:8080/grisu-ws/services/grisu",
+				// "https://ngportaldev.vpac.org/grisu-ws/services/grisu",
+				"Local", username, password);
+
+		ServiceInterface si = null;
+		si = ServiceInterfaceFactory.createInterface(loginParams);
+
+		DtoJobs test = si.ps(true);
+
+		GrisuRegistry registry = GrisuRegistryManager.getDefault(si);
+
+		ApplicationInformation appInfo = registry
+				.getApplicationInformation("java");
+		for (String version : appInfo
+				.getAllAvailableVersionsForFqan("/ARCS/NGAdmin")) {
+			System.out.println(version);
+		}
+
+	}
+
+}
