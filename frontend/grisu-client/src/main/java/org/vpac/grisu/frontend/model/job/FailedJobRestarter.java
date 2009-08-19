7910de2915d49bd8526858c979110167c9092e07
Markus Binsteiner <markus@avus.(none)>
2009-08-19 22:37:04 +1000

    More  work on multipartjob support and blender support, added derby as database option

---
+++ frontend/grisu-client/src/main/java/org/vpac/grisu/frontend/model/job/FailedJobRestarter.java
@@ -0,0 +1,9 @@
+package org.vpac.grisu.frontend.model.job;
+
+import org.vpac.grisu.control.exceptions.JobSubmissionException;
+
+public interface FailedJobRestarter {
+	
+	public void restartJob(JobObject job) throws JobSubmissionException;
+
+}
