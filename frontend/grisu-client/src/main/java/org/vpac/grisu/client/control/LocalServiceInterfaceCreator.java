487e7f8c4249f8af9a5588ba23d25113f80442f3
Markus Binsteiner <markus@avus.(none)>
2009-08-31 19:24:04 +1000

    Changed debug output.

--- frontend/grisu-client/src/main/java/org/vpac/grisu/client/control/LocalServiceInterfaceCreator.java
+++ frontend/grisu-client/src/main/java/org/vpac/grisu/client/control/LocalServiceInterfaceCreator.java
@@ -33,7 +33,7 @@ public class LocalServiceInterfaceCreator implements ServiceInterfaceCreator {
 			myLogger.warn("Could not find local service interface class.");
 			e.printStackTrace();
 			throw new ServiceInterfaceException(
-					"Could not find LocalServiceInterface class. Probably grisu-local-backend.jar is not in the classpath.",
+					"Could not find LocalServiceInterface class. Probably local-backend.jar is not in the classpath.",
 					e);
 		}
 
