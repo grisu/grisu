3318737b48fc38cb28bc8200f97f12c2d3748ffc
Markus Binsteiner <markus.binsteiner@arcs.org.au>
2009-08-31 10:32:22 +1000

    Moved grisu-grid-tests to grisu-clients maven project.

--- frontend/grisu-client/src/main/java/org/vpac/grisu/frontend/control/login/LoginException.java
+++ frontend/grisu-client/src/main/java/org/vpac/grisu/frontend/control/login/LoginException.java
@@ -5,5 +5,9 @@ public class LoginException extends Exception {
 	public LoginException(final String message, final Exception e) {
 		super(message, e);
 	}
+	
+	public LoginException(String message) {
+		super(message);
+	}
 
 }
