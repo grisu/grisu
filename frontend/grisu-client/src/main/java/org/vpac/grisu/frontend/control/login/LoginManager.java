a70ce90b8316125cb81d9995f7add2c4f41f99f9
Markus Binsteiner <markus@avus.(none)>
2009-09-02 20:20:01 +1000

    Added automated copying of .globus/certicates into place.

--- frontend/grisu-client/src/main/java/org/vpac/grisu/frontend/control/login/LoginManager.java
+++ frontend/grisu-client/src/main/java/org/vpac/grisu/frontend/control/login/LoginManager.java
@@ -14,6 +14,7 @@ import org.vpac.grisu.control.ServiceInterface;
 import org.vpac.grisu.control.exceptions.ServiceInterfaceException;
 import org.vpac.grisu.settings.Environment;
 import org.vpac.grisu.utils.GrisuPluginFilenameFilter;
+import org.vpac.security.light.control.CertificateFiles;
 
 import au.org.arcs.jcommons.dependencies.ClasspathHacker;
 import au.org.arcs.jcommons.dependencies.DependencyManager;
@@ -59,6 +60,12 @@ public class LoginManager {
 			// TODO Auto-generated catch block
 			e2.printStackTrace();
 			throw new RuntimeException(e2);
+		}
+		
+		try {
+			CertificateFiles.copyCACerts();
+		} catch (Exception e1) {
+			e1.printStackTrace();
 		}
 
 		// do the cacert thingy
