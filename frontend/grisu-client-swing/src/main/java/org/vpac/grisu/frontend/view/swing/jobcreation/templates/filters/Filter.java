3c151af5300e8e43c3904a615ce4cdb8b39c9bd6
Markus Binsteiner <m.binsteiner@auckland.ac.nz>
2010-08-20 15:01:05 +0200

    Refactoring because of maven module reorganisation.

--- frontend/grisu-client-swing/src/main/java/org/vpac/grisu/frontend/view/swing/jobcreation/templates/filters/Filter.java
+++
@@ -1,9 +0,0 @@
-package org.vpac.grisu.frontend.view.swing.jobcreation.templates.filters;
-
-public interface Filter {
-
-	public abstract void config(String[] config);
-
-	public abstract String filter(String value);
-
-}
