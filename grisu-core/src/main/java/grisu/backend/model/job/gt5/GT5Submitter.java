7819da8b8f0ff4ce5b094d887adfcc8032982343
Yuriy Halytskyy <yuriy.halytskyy@gmail.com>
2011-05-05 15:14:14 +1200

    gt5 jobtype bug

--- grisu-core/src/main/java/grisu/backend/model/job/gt5/GT5Submitter.java
+++ grisu-core/src/main/java/grisu/backend/model/job/gt5/GT5Submitter.java
@@ -174,14 +174,14 @@ public class GT5Submitter extends JobSubmitter {
 
 		if (processorCount > 1) {
 
-			if (jobTypeString == null) {
+			if (StringUtils.isNotBlank(jobTypeString)) {
 				jobType = new NameOpValue("job_type", NameOpValue.EQ, "mpi");
 			} else {
 				jobType = new NameOpValue("job_type", NameOpValue.EQ,
 						jobTypeString);
 			}
 		} else {
-			if (jobTypeString == null) {
+			if (StringUtils.isNotBlank(jobTypeString)) {
 				jobType = new NameOpValue("job_type", NameOpValue.EQ, "single");
 			} else {
 				jobType = new NameOpValue("job_type", NameOpValue.EQ,
