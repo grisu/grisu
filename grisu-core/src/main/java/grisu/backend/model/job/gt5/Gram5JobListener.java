cff40e460cb7737b35335efdab51e6de03776981
Markus Binsteiner <m.binsteiner@auckland.ac.nz>
2011-09-14 10:06:22 +0200

    Making Gram5JobListener threadsafe.

--- grisu-core/src/main/java/grisu/backend/model/job/gt5/Gram5JobListener.java
+++ grisu-core/src/main/java/grisu/backend/model/job/gt5/Gram5JobListener.java
@@ -1,46 +1,45 @@
 package grisu.backend.model.job.gt5;
 
+import java.util.Collections;
 import java.util.HashMap;
-
-import org.globus.gram.GramJob;
-import org.globus.gram.GramJobListener;
+import java.util.Map;
 
 import org.apache.log4j.Logger;
+import org.globus.gram.GramJob;
+import org.globus.gram.GramJobListener;
 
 public class Gram5JobListener implements GramJobListener{
 
 	static final Logger myLogger = Logger.getLogger(GT5Submitter.class
 			.getName());
-	
-	private static Gram5JobListener l;
-	private HashMap<String,Integer> statuses; 
-	private HashMap<String,Integer> errors; 
-	
-	private Gram5JobListener(){
-		statuses = new HashMap<String,Integer>();
-		errors = new HashMap<String,Integer>();
-	}
-	
+
+	private static Gram5JobListener l = new Gram5JobListener();
 	public static Gram5JobListener getJobListener(){
-		if (l == null){
-			l = new Gram5JobListener();
-		}
 		return l;
 	}
 
+	private final Map<String, Integer> statuses;
 
-	public void statusChanged(GramJob job) {
-        myLogger.debug("job status changed to " + job.getStatus());
-		statuses.put( job.getIDAsString(),job.getStatus());
-		errors.put(job.getIDAsString(), job.getError());
+	private final Map<String, Integer> errors;
+
+	private Gram5JobListener(){
+		statuses = Collections.synchronizedMap(new HashMap<String, Integer>());
+		errors = Collections.synchronizedMap(new HashMap<String, Integer>());
 	}
-	
+
+
+	public Integer getError(String handle){
+		return errors.get(handle);
+	}
+
 	public Integer getStatus(String handle){
 		return statuses.get(handle);
 	}
-	
-	public Integer getError(String handle){
-		return errors.get(handle);
+
+	public void statusChanged(GramJob job) {
+		myLogger.debug("job status changed to " + job.getStatus());
+		statuses.put( job.getIDAsString(),job.getStatus());
+		errors.put(job.getIDAsString(), job.getError());
 	}
 
 }
