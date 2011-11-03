022f21a01f591ec5a0e51e601819ad228ef71884
Markus Binsteiner <m.binsteiner@auckland.ac.nz>
2011-11-03 14:12:01 +1300

    Fixed StatusObject wait threshold check.

--- grisu-commons/src/main/java/grisu/model/status/StatusObject.java
+++ grisu-commons/src/main/java/grisu/model/status/StatusObject.java
@@ -101,7 +101,7 @@ public class StatusObject {
 				public void run() {
 					while (!getStatus().isFinished()) {
 						try {
-							if (((new Date().getTime() - startMonitoringTime) * 1000) < thresholdInSeconds) {
+							if ((new Date().getTime() - startMonitoringTime) < (thresholdInSeconds * 1000)) {
 								throw new InterruptedException(
 										"Threshold for task monitoring exceeded. Not waiting any longer...");
 							}
