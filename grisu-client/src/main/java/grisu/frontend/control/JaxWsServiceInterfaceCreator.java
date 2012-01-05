59b5b2cbddaed445197c00a4e93f7329f315d674
Markus Binsteiner <m.binsteiner@auckland.ac.nz>
2012-01-05 15:26:17 +1300

    Code cleanup.

--- grisu-client/src/main/java/grisu/frontend/control/JaxWsServiceInterfaceCreator.java
+++ grisu-client/src/main/java/grisu/frontend/control/JaxWsServiceInterfaceCreator.java
@@ -74,7 +74,7 @@ public class JaxWsServiceInterfaceCreator implements ServiceInterfaceCreator {
 			final QName serviceName = new QName("http://api.grisu",
 					"GrisuService");
 			final QName portName = new QName("http://api.grisu",
-			// "ServiceInterfaceSOAPPort");
+					// "ServiceInterfaceSOAPPort");
 					"ServiceInterfacePort");
 
 			Service s;
@@ -119,8 +119,8 @@ public class JaxWsServiceInterfaceCreator implements ServiceInterfaceCreator {
 
 			// just to be sure, I'll keep that in there as well...
 			bp.getRequestContext()
-					.put("com.sun.xml.internal.ws.transport.http.client.streaming.chunk.size",
-							new Integer(4096));
+			.put("com.sun.xml.internal.ws.transport.http.client.streaming.chunk.size",
+					new Integer(4096));
 
 			bp.getRequestContext().put(BindingProvider.USERNAME_PROPERTY,
 					username);
@@ -145,88 +145,5 @@ public class JaxWsServiceInterfaceCreator implements ServiceInterfaceCreator {
 
 	}
 
-	// private SSLSocketFactory createSocketFactory(String interfaceUrl)
-	// throws ServiceInterfaceException {
-	// // Technique similar to
-	// // http://juliusdavies.ca/commons-ssl/TrustExample.java.html
-	// HttpSecureProtocol protocolSocketFactory;
-	// try {
-	// protocolSocketFactory = new HttpSecureProtocol();
-	//
-	// TrustMaterial trustMaterial = null;
-	//
-	// // "/thecertificate.cer" can be PEM or DER (raw ASN.1). Can even
-	// // be several PEM certificates in one file.
-	//
-	// String cacertFilename = System.getProperty("grisu.cacert");
-	// URL cacertURL = null;
-	//
-	// try {
-	// if (cacertFilename != null && !"".equals(cacertFilename)) {
-	// cacertURL = JaxWsServiceInterfaceCreator.class
-	// .getResource("/" + cacertFilename);
-	// if (cacertURL != null) {
-	// myLogger.debug("Using cacert " + cacertFilename
-	// + " as configured in the -D option.");
-	// }
-	// }
-	// } catch (Exception e) {
-	// // doesn't matter
-	// myLogger
-	// .debug("Couldn't find specified cacert. Using default one.");
-	// }
-	//
-	// if (cacertURL == null) {
-	//
-	// cacertFilename = new CaCertManager()
-	// .getCaCertNameForServiceInterfaceUrl(interfaceUrl);
-	// if (cacertFilename != null && cacertFilename.length() > 0) {
-	// myLogger
-	// .debug("Found url in map. Trying to use this cacert file: "
-	// + cacertFilename);
-	// cacertURL = JaxWsServiceInterfaceCreator.class
-	// .getResource("/" + cacertFilename);
-	// if (cacertURL == null) {
-	// myLogger
-	// .debug("Didn't find cacert. Using the default one.");
-	// // use the default one
-	// cacertURL = JaxWsServiceInterfaceCreator.class
-	// .getResource("/cacert.pem");
-	// } else {
-	// myLogger.debug("Found cacert. Using it. Good.");
-	// }
-	// } else {
-	// myLogger
-	// .debug("Didn't find any configuration for a special cacert. Using the default one.");
-	// // use the default one
-	// cacertURL = JaxWsServiceInterfaceCreator.class
-	// .getResource("/cacert.pem");
-	// }
-	//
-	// }
-	//
-	// trustMaterial = new TrustMaterial(cacertURL);
-	//
-	// // We can use setTrustMaterial() instead of addTrustMaterial()
-	// // if we want to remove
-	// // HttpSecureProtocol's default trust of TrustMaterial.CACERTS.
-	// protocolSocketFactory.addTrustMaterial(trustMaterial);
-	//
-	// // Maybe we want to turn off CN validation (not recommended!):
-	// protocolSocketFactory.setCheckHostname(false);
-	//
-	// Protocol protocol = new Protocol("https",
-	// (ProtocolSocketFactory) protocolSocketFactory, 443);
-	// Protocol.registerProtocol("https", protocol);
-	//
-	// return protocolSocketFactory;
-	// } catch (Exception e1) {
-	// // TODO Auto-generated catch block
-	// // e1.printStackTrace();
-	// throw new ServiceInterfaceException(
-	// "Unspecified error while trying to establish secure connection.",
-	// e1);
-	// }
-	// }
 
 }
