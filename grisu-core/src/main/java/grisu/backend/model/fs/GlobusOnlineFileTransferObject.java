9187a902f0e7de1a657babafb180aef58b9a5a23
Markus Binsteiner <m.binsteiner@auckland.ac.nz>
2012-01-06 15:13:33 +1300

    Disabled GO integration for now.

--- grisu-core/src/main/java/grisu/backend/model/fs/GlobusOnlineFileTransferObject.java
+++ grisu-core/src/main/java/grisu/backend/model/fs/GlobusOnlineFileTransferObject.java
@@ -1,63 +1,59 @@
 package grisu.backend.model.fs;
 
-import grisu.backend.model.RemoteFileTransferObject;
-import nz.org.nesi.goji.control.GlobusOnlineUserSession;
-import nz.org.nesi.goji.exceptions.CommandException;
-import nz.org.nesi.goji.model.Transfer;
 
-public class GlobusOnlineFileTransferObject implements RemoteFileTransferObject {
+public class GlobusOnlineFileTransferObject {
 
-	private final GlobusOnlineUserSession go;
-	private final String source;
-	private final String target;
-
-	private Transfer transfer;
-
-	public GlobusOnlineFileTransferObject(GlobusOnlineUserSession go,
-			String source, String target) {
-		this.go = go;
-		this.source = source;
-		this.target = target;
-
-	}
-
-	public String getId() {
-		return transfer.getTaskId();
-	}
-
-	public Exception getPossibleException() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public String getPossibleExceptionMessage() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public boolean isFailed() {
-		// TODO
-		return false;
-	}
-
-	public void joinFileTransfer() {
-		transfer.waitForTransferToFinish();
-
-	}
-
-	public void startTransfer(boolean waitForTransferToFinish) {
-		// TODO Auto-generated method stub
-
-		try {
-			transfer = go.transfer(source, target);
-		} catch (CommandException e) {
-			throw new RuntimeException(e);
-		}
-
-		if (waitForTransferToFinish) {
-			joinFileTransfer();
-		}
-
-	}
+	// private final GlobusOnlineUserSession go;
+	// private final String source;
+	// private final String target;
+	//
+	// private Transfer transfer;
+	//
+	// public GlobusOnlineFileTransferObject(GlobusOnlineUserSession go,
+	// String source, String target) {
+	// this.go = go;
+	// this.source = source;
+	// this.target = target;
+	//
+	// }
+	//
+	// public String getId() {
+	// return transfer.getTaskId();
+	// }
+	//
+	// public Exception getPossibleException() {
+	// // TODO Auto-generated method stub
+	// return null;
+	// }
+	//
+	// public String getPossibleExceptionMessage() {
+	// // TODO Auto-generated method stub
+	// return null;
+	// }
+	//
+	// public boolean isFailed() {
+	// // TODO
+	// return false;
+	// }
+	//
+	// public void joinFileTransfer() {
+	// transfer.waitForTransferToFinish();
+	//
+	// }
+	//
+	// public void startTransfer(boolean waitForTransferToFinish) {
+	// // TODO Auto-generated method stub
+	//
+	// try {
+	// transfer = go.transfer(source, target);
+	// } catch (CommandException e) {
+	// throw new RuntimeException(e);
+	// }
+	//
+	// if (waitForTransferToFinish) {
+	// joinFileTransfer();
+	// }
+	//
+	// }
 
 }
--- grisu-core/src/main/java/grisu/backend/model/fs/GlobusOnlineFileTransferPlugin.java
+++ grisu-core/src/main/java/grisu/backend/model/fs/GlobusOnlineFileTransferPlugin.java
@@ -1,38 +1,28 @@
 package grisu.backend.model.fs;
 
-import grisu.backend.model.RemoteFileTransferObject;
-import grisu.backend.model.User;
-import grisu.backend.model.UserInfoManager;
-import grisu.control.exceptions.RemoteFileSystemException;
-import grisu.jcommons.interfaces.InfoManager;
-import grith.jgrith.credential.Credential;
-import grith.jgrith.credential.WrappedGssCredential;
-import nz.org.nesi.goji.control.GlobusOnlineUserSession;
 
-import org.ietf.jgss.GSSCredential;
+public class GlobusOnlineFileTransferPlugin {
 
-public class GlobusOnlineFileTransferPlugin implements FileTransferPlugin {
-
-	private final String go_user = "markus";
-	private final InfoManager im;
-
-	private final GlobusOnlineUserSession go;
-
-	public GlobusOnlineFileTransferPlugin(User user) {
-
-		im = new UserInfoManager(user);
-
-		GSSCredential gssCred = user.getCredential().getCredential();
-		Credential cred = new WrappedGssCredential(gssCred);
-		go = new GlobusOnlineUserSession(go_user, cred, im);
-
-	}
-
-	public RemoteFileTransferObject copySingleFile(String source,
-			String target, boolean overwrite) throws RemoteFileSystemException {
-
-		GlobusOnlineFileTransferObject to = new GlobusOnlineFileTransferObject(
-				go, source, target);
-		return to;
-	}
+	// private final String go_user = "markus";
+	// private final InfoManager im;
+	//
+	// private final GlobusOnlineUserSession go;
+	//
+	// public GlobusOnlineFileTransferPlugin(User user) {
+	//
+	// im = new UserInfoManager(user);
+	//
+	// GSSCredential gssCred = user.getCredential().getCredential();
+	// Credential cred = new WrappedGssCredential(gssCred);
+	// go = new GlobusOnlineUserSession(go_user, cred, im);
+	//
+	// }
+	//
+	// public RemoteFileTransferObject copySingleFile(String source,
+	// String target, boolean overwrite) throws RemoteFileSystemException {
+	//
+	// GlobusOnlineFileTransferObject to = new GlobusOnlineFileTransferObject(
+	// go, source, target);
+	// return to;
+	// }
 }
