23c71a0b5d80a04778cd4b1ba16875617e32cfb0
Markus Binsteiner <m.binsteiner@auckland.ac.nz>
2012-05-16 10:59:24 +1200

    Changed API version. Added commoncliparser.

--- grisu-commons/src/main/java/grisu/control/ServiceInterface.java
+++ grisu-commons/src/main/java/grisu/control/ServiceInterface.java
@@ -55,7 +55,7 @@ import javax.xml.bind.annotation.XmlMimeType;
 @WebService(targetNamespace = "http://api.grisu", serviceName = "GrisuService")
 public interface ServiceInterface {
 
-	public static final int API_VERSION = 15;
+	public static final int API_VERSION = 16;
 
 	public static final String VIRTUAL_GRID_PROTOCOL_NAME = "grid";
 	public static String GRISU_JOB_FILE_NAME = ".grisujob";
