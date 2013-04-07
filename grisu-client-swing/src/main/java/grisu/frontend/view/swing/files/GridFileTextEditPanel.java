fbe1265cddeac89c12336218f35853f343c84f51
Markus Binsteiner <m.binsteiner@auckland.ac.nz>
2013-04-08 10:48:57 +1200

    Added general purpose text editor file

--- grisu-client-swing/src/main/java/grisu/frontend/view/swing/files/GridFileTextEditPanel.java
+++ grisu-client-swing/src/main/java/grisu/frontend/view/swing/files/GridFileTextEditPanel.java
@@ -3,200 +3,107 @@ package grisu.frontend.view.swing.files;
 import grisu.control.ServiceInterface;
 import grisu.control.exceptions.RemoteFileSystemException;
 import grisu.frontend.control.clientexceptions.FileTransactionException;
+import grisu.frontend.view.swing.files.open.FileDialogManager;
+import grisu.frontend.view.swing.files.open.GridFileHolder;
+import grisu.frontend.view.swing.utils.DisabledGlassPane;
 import grisu.model.FileManager;
 import grisu.model.GrisuRegistryManager;
 import grisu.model.dto.GridFile;
 
 import java.awt.BorderLayout;
-import java.awt.Window;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
 import java.io.File;
 import java.io.IOException;
-import java.util.Map;
 
+import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.JPopupMenu;
-import javax.swing.ListSelectionModel;
-import javax.swing.SwingUtilities;
-import javax.swing.WindowConstants;
 
 import org.apache.commons.io.FileUtils;
-import org.apache.commons.lang3.StringUtils;
 import org.gjt.sp.jedit.Mode;
 import org.gjt.sp.jedit.syntax.ModeProvider;
 import org.gjt.sp.jedit.textarea.StandaloneTextArea;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.google.common.collect.Maps;
+public class GridFileTextEditPanel extends JPanel implements GridFileHolder {
 
-public class GridFileTextEditPanel extends JPanel {
-	
-	public static final Logger myLogger = LoggerFactory.getLogger(GridFileTextEditPanel.class);
+	public static final Logger myLogger = LoggerFactory
+			.getLogger(GridFileTextEditPanel.class);
 	private static final long serialVersionUID = 1L;
-	
-	public static final String FILE_DIALOG_KEY = "file_dialog";
+
 	public static final String DEFAULT_FILE_DIALOG_ALIAS = "default";
-	
-	private static Map<String, GridFileSelectionDialog> dialogs = Maps
-			.newConcurrentMap();
-	
-	private synchronized static void createSingletonFileDialog(Window owner,
-			ServiceInterface si, String dialogAlias) {
 
-		if (dialogs.get(dialogAlias) == null) {
-			String startUrl = GrisuRegistryManager
-					.getDefault(si)
-					.getHistoryManager()
-					.getLastEntry(
-							dialogAlias + "_" + FILE_DIALOG_KEY);
-
-			if (StringUtils.isBlank(startUrl)) {
-				startUrl = new File(System.getProperty("user.home")).toURI()
-						.toString();
-			} else if (!FileManager.isLocal(startUrl)) {
-				try {
-					if (!si.isFolder(startUrl)) {
-						startUrl = new File(System.getProperty("user.home"))
-						.toURI().toString();
-					}
-				} catch (final RemoteFileSystemException e) {
-					myLogger.debug("Can't load file: "+startUrl, e);
-					startUrl = new File(System.getProperty("user.home"))
-					.toURI().toString();
-				}
-			}
-			final GridFileSelectionDialog dialog = new GridFileSelectionDialog(
-					owner, si);
-
-			dialog.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-			dialogs.put(dialogAlias, dialog);
-		}
-	}
-	
-	private StandaloneTextArea textArea;
+	private GrisuTextArea textArea;
 	private boolean documentUnsaved = false;
 	private ServiceInterface si;
 	private FileManager fm;
-	
-	private String fileDialogAlias = null;
-	
-	private GridFile currentFile;
-	
-	private PropertyChangeSupport pcs = new PropertyChangeSupport(this);
-	
-	public GridFileSelectionDialog getFileDialog(String dialogName) {
+	private FileDialogManager fdm;
 
-		if (dialogs.get(dialogName) == null) {
-			if (si == null) {
-				throw new IllegalStateException(
-						"File dialog not initialized yet.");
-			}
-			createSingletonFileDialog(SwingUtilities.getWindowAncestor(this),
-					si, dialogName);
-		}
-		return dialogs.get(dialogName);
-	}
-	
-	public GridFileTextEditPanel() {
-		this(DEFAULT_FILE_DIALOG_ALIAS);
-	}
+	private String fileDialogAlias = null;
+
+	private GridFile currentFile;
+
+	private PropertyChangeSupport pcs = new PropertyChangeSupport(this);
+
+	private DisabledGlassPane glassPane = new DisabledGlassPane();
 
 	/**
 	 * Create the panel.
 	 */
-	public GridFileTextEditPanel(String fileDialogAlias) {
+	public GridFileTextEditPanel(String fileDialogAlias, JFrame frame) {
 		setLayout(new BorderLayout(0, 0));
+
+		frame.setGlassPane(glassPane);
+
 		add(getStandaloneTextArea(), BorderLayout.CENTER);
-		
+
 		this.fileDialogAlias = fileDialogAlias;
 	}
-	
-	public String getFileDialogAlias() {
-		return fileDialogAlias;
+
+	public void activateGlassPane(String msg) {
+		glassPane.activate(msg);
 	}
-	
-	public void setFileDialogAlias(String alias) {
-		this.fileDialogAlias = alias;
+
+	public void deactivateGlassPane() {
+		glassPane.deactivate();
 	}
-	
+
 	public void addPropertyChangeListener(PropertyChangeListener l) {
 		pcs.addPropertyChangeListener(l);
 	}
-	
-	public void removePropertyChangeListener(PropertyChangeListener l) {
-		pcs.removePropertyChangeListener(l);
-	}
-	
-	public GridFileSelectionDialog getFileDialog() {
-		return getFileDialog(fileDialogAlias);
-	}
-	
+
 	public void askUserForFile() {
-		
-		GridFile file = popupFileDialogAndAskForFile();
-		
+
+		GridFile file = fdm.popupFileDialogAndAskForFile(fileDialogAlias);
+
 		setFile(file);
-		
+
 	}
-	
-	protected GridFile popupFileDialogAndAskForFile() {
 
-		getFileDialog().setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-		getFileDialog().centerOnOwner();
-		getFileDialog().setVisible(true);
-
-		final GridFile file = getFileDialog().getSelectedFile();
-
-		// final Set<GridFile> currentDirs =
-		// getFileDialog().getCurrentDirectories();
-		// if (currentDirs != null) {
-		//
-		// hm.addHistoryEntry(templateName + "_" +
-		// FILE_DIALOG_LAST_DIRECTORY_KEY,
-		// currentDir.getUrl());
-		// }
-
-		return file;
+	public GridFile getFile() {
+		return currentFile;
 	}
-	
-	public void setTextMode(String modeName) {
-		final Mode mode = new Mode(modeName);
-		mode.setProperty("file", mode + ".xml");
-		ModeProvider.instance.addMode(mode);
-		getStandaloneTextArea().getBuffer().setMode(mode);
+
+	public GridFileSelectionDialog getFileDialog() {
+
+		return fdm.getFileDialog(fileDialogAlias);
+	}
+
+	public String getFileDialogAlias() {
+		return fileDialogAlias;
 	}
 
 	private StandaloneTextArea getStandaloneTextArea() {
 		if (textArea == null) {
-			textArea = StandaloneTextArea.createTextArea();
-			
-			final Mode mode = new Mode("text");
-			mode.setProperty("file", "text.xml");
-			ModeProvider.instance.addMode(mode);
-			textArea.getBuffer().setMode(mode);
-			
-
-			//textArea.setRightClickPopup(po);
-			JPopupMenu p = new JPopupMenu();
-			textArea.setRightClickPopup(p);
-			
-			textArea.addMenuItem("undo", "Undo");
-			textArea.addMenuItem("redo", "Redo");
-			p.addSeparator();
-			textArea.addMenuItem("cut", "Cut");
-			textArea.addMenuItem("copy", "Copy");
-			textArea.addMenuItem("paste", "Paste");
-			
-			textArea.setRightClickPopupEnabled(true);
+			textArea = new GrisuTextArea();
 
 			textArea.addKeyListener(new KeyAdapter() {
 				@Override
 				public void keyReleased(KeyEvent e) {
-					if ( documentUnsaved == false ) {
+					if (documentUnsaved == false) {
 						documentUnsaved = true;
 						pcs.firePropertyChange("documentUnsaved", false, true);
 					}
@@ -206,82 +113,110 @@ public class GridFileTextEditPanel extends JPanel {
 		}
 		return textArea;
 	}
-	
-	public void setServiceInterface(ServiceInterface si) {
-		this.si = si;
-		this.fm = GrisuRegistryManager.getDefault(
-				si).getFileManager();
+
+	public String getText() {
+		return getStandaloneTextArea().getText();
+	}
+
+	public boolean isDocumentUnsaved() {
+		return documentUnsaved;
+	}
+
+	public void removePropertyChangeListener(PropertyChangeListener l) {
+		pcs.removePropertyChangeListener(l);
+	}
+
+	public void setFile(final GridFile file) {
+
+		if (file == null) {
+			return;
+		}
+
+		if (si == null) {
+			myLogger.debug("TextEditPanel not initialized yet, not loading file: "
+					+ file.getUrl());
+		}
+
+		activateGlassPane("Loading file: " + file.getName());
+
+		Thread t = new Thread() {
+			public void run() {
+
+				try {
+
+					File localfile = null;
+					try {
+						localfile = fm.downloadFile(file.getUrl());
+					} catch (final FileTransactionException e1) {
+						myLogger.error("Can't download file: " + file.getUrl(),
+								e1);
+						return;
+					}
+
+					String text;
+					try {
+						text = FileUtils.readFileToString(localfile);
+					} catch (final IOException e) {
+						myLogger.error("Can't read file to string: "
+								+ localfile.getAbsolutePath());
+						return;
+					}
+
+					setTextAreaText(text);
+
+					if (documentUnsaved == true) {
+						documentUnsaved = false;
+						pcs.firePropertyChange("documentUnsaved", true, false);
+					}
+
+					GridFile old = currentFile;
+					currentFile = file;
+					pcs.firePropertyChange("currentFile", currentFile, old);
+
+				} finally {
+					deactivateGlassPane();
+				}
+			}
+		};
+		t.setName("OpenFileDownloadThread");
+		t.start();
 
 	}
-	
+
 	public void setFile(String path_or_url) throws RemoteFileSystemException {
-		
-		if ( si == null ) {
-			myLogger.debug("TextEditPanel not initialized yet, not loading file: "+path_or_url);
+
+		if (si == null) {
+			myLogger.debug("TextEditPanel not initialized yet, not loading file: "
+					+ path_or_url);
 		}
-		
+
 		GridFile file = fm.createGridFile(path_or_url);
 		setFile(file);
 	}
-	
+
+	public void setFileDialogAlias(String alias) {
+		this.fileDialogAlias = alias;
+	}
+
+	public void setServiceInterface(ServiceInterface si) {
+		this.si = si;
+		this.fm = GrisuRegistryManager.getDefault(si).getFileManager();
+		this.fdm = FileDialogManager.getDefault(si, this);
+
+	}
+
 	private void setTextAreaText(String text) {
-		if ( text == null ) {
+		if (text == null) {
 			text = "";
 		}
 		getStandaloneTextArea().setText(text);
 
 	}
-	
-	public String getText() {
-		return getStandaloneTextArea().getText();
-	}
 
-	
-	public void setFile(GridFile file) {
-		
-		if ( file == null ) {
-			return;
-		}
-		
-		if ( si == null ) {
-			myLogger.debug("TextEditPanel not initialized yet, not loading file: "+file.getUrl());
-		}
-		
-
-		File localfile = null;
-		try {
-			localfile = fm.downloadFile(file.getUrl());
-		} catch (final FileTransactionException e1) {
-			myLogger.error("Can't download file: "+file.getUrl(), e1);
-			return;
-		}
-
-		String text;
-		try {
-			text = FileUtils.readFileToString(localfile);
-		} catch (final IOException e) {
-			myLogger.error("Can't read file to string: "+localfile.getAbsolutePath());
-			return;
-		}
-
-		setTextAreaText(text);
-		
-		if ( documentUnsaved == true ) {
-			documentUnsaved = false;
-			pcs.firePropertyChange("documentUnsaved", true, false);
-		}	
-		
-		GridFile old = currentFile;
-		currentFile = file;
-		pcs.firePropertyChange("currentFile", currentFile, old);
-
-	}
-	
-	public GridFile getFile() {
-		return currentFile;
-	}
-	
-	public boolean isDocumentUnsaved() {
-		return documentUnsaved;
+	public void setTextMode(String modeName) {
+		final Mode mode = new Mode(modeName);
+		mode.setProperty("file", mode + ".xml");
+		ModeProvider.instance.addMode(mode);
+		getStandaloneTextArea().getBuffer().setMode(mode);
 	}
 }
---
+++ grisu-client-swing/src/main/java/grisu/frontend/view/swing/files/open/FileDialogManager.java
@@ -0,0 +1,160 @@
+package grisu.frontend.view.swing.files.open;
+
+import grisu.control.ServiceInterface;
+import grisu.control.exceptions.RemoteFileSystemException;
+import grisu.frontend.view.swing.files.GridFileSelectionDialog;
+import grisu.frontend.view.swing.files.contextMenu.OpenAction;
+import grisu.model.FileManager;
+import grisu.model.GrisuRegistryManager;
+import grisu.model.dto.GridFile;
+
+import java.awt.Component;
+import java.awt.Window;
+import java.awt.event.ActionEvent;
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.swing.AbstractAction;
+import javax.swing.ImageIcon;
+import javax.swing.ListSelectionModel;
+import javax.swing.SwingUtilities;
+import javax.swing.WindowConstants;
+
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.collect.Maps;
+
+public class FileDialogManager {
+	
+	public class OpenFileAction extends AbstractAction {
+		
+		private final FileDialogManager fdm;
+		private final GridFileHolder gfh;
+		private final String dialogAlias;
+		
+	    public OpenFileAction(FileDialogManager fdm, GridFileHolder h, String dialogAlias) {
+	        super();
+	        this.fdm = fdm;
+	        this.gfh = h;
+	        this.dialogAlias = dialogAlias;
+	        putValue(SMALL_ICON, new ImageIcon(FileDialogManager.class.getResource("/sun/print/resources/oneside.png")));
+	        putValue(NAME, "Open");
+	        putValue(SHORT_DESCRIPTION, "Open file");
+	    }
+	    public void actionPerformed(ActionEvent e) {
+	        GridFile file = this.fdm.popupFileDialogAndAskForFile(dialogAlias);
+	        gfh.setFile(file);
+	    }
+	}
+	
+	public static final Component defaultRootComponent = null;
+	
+	public static final String FILE_DIALOG_KEY = "file_dialog";
+
+	
+	static final Logger myLogger = LoggerFactory
+			.getLogger(FileDialogManager.class.getName());
+	
+	private static Map<ServiceInterface, FileDialogManager> cachedRegistries = new HashMap<ServiceInterface, FileDialogManager>();
+	
+	public static FileDialogManager getDefault(final ServiceInterface si) {
+		return getDefault(si, null);
+	}
+	
+	private synchronized static void createSingletonFileDialog(Window owner,
+			ServiceInterface si, String dialogAlias) {
+
+		if (dialogs.get(dialogAlias) == null) {
+			String startUrl = GrisuRegistryManager
+					.getDefault(si)
+					.getHistoryManager()
+					.getLastEntry(
+							dialogAlias + "_" + FILE_DIALOG_KEY);
+
+			if (StringUtils.isBlank(startUrl)) {
+				startUrl = new File(System.getProperty("user.home")).toURI()
+						.toString();
+			} else if (!FileManager.isLocal(startUrl)) {
+				try {
+					if (!si.isFolder(startUrl)) {
+						startUrl = new File(System.getProperty("user.home"))
+						.toURI().toString();
+					}
+				} catch (final RemoteFileSystemException e) {
+					myLogger.debug("Can't load file: "+startUrl, e);
+					startUrl = new File(System.getProperty("user.home"))
+					.toURI().toString();
+				}
+			}
+			final GridFileSelectionDialog dialog = new GridFileSelectionDialog(
+					owner, si);
+
+			dialog.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+			dialogs.put(dialogAlias, dialog);
+		}
+	}
+
+	public static FileDialogManager getDefault(final ServiceInterface si, Component component) {
+
+		if (si == null) {
+			throw new RuntimeException(
+					"ServiceInterface not initialized yet. Can't get default filedialog registry...");
+		}
+
+		synchronized (si) {
+			if (cachedRegistries.get(si) == null) {
+				final FileDialogManager m = new FileDialogManager(si, component);
+				cachedRegistries.put(si, m);
+			}
+		}
+
+		return cachedRegistries.get(si);
+	}
+	
+	private final ServiceInterface si;
+	private static Map<String, GridFileSelectionDialog> dialogs = Maps
+			.newConcurrentMap();
+
+	private final Window rootComponent;
+	
+	public FileDialogManager(ServiceInterface si, Component comp) {
+		this.si = si;
+		if ( comp == null ) {
+			comp = defaultRootComponent;
+		}
+		this.rootComponent = SwingUtilities.getWindowAncestor(comp);
+	}
+	
+	public GridFileSelectionDialog getFileDialog(String dialogName) {
+
+		if (dialogs.get(dialogName) == null) {
+			if (si == null) {
+				throw new IllegalStateException(
+						"File dialog not initialized yet.");
+			}
+			createSingletonFileDialog(rootComponent,
+					si, dialogName);
+		}
+		return dialogs.get(dialogName);
+	}
+	
+	public GridFile popupFileDialogAndAskForFile(String alias) {
+
+		getFileDialog(alias).setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		getFileDialog(alias).centerOnOwner();
+		getFileDialog(alias).setVisible(true);
+
+		final GridFile file = getFileDialog(alias).getSelectedFile();
+
+		return file;
+	}
+	
+	public OpenFileAction createOpenAction(GridFileHolder gfh, String dialogAlias) {
+		OpenFileAction oa = new OpenFileAction(this, gfh, dialogAlias);
+		return oa;
+	}
+
+}
