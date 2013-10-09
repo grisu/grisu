b30eb7d8872b07a7633127cbe0066a6bd66f3cf3
Markus Binsteiner <m.binsteiner@auckland.ac.nz>
2013-10-09 15:43:51 +1300

    Windows examples

--- grisu-client/examples/monitor.groovy
+++
@@ -1,9 +0,0 @@
-import grisu.frontend.control.MonitoringConfig
-
-monitor1 = new MonitoringConfig(
-  applications : ['Java'],
-  filesToDownload : ['output.zip'],
-  targetDir : 'C:\\Users\\Markus\\Desktop\\downloads',
-  deleteJob : true,
-  postProcessCommand : ["cmd.exe", "\\c", "C:\\Users\\Markus\\Desktop\\test_script.bat"]
-)
